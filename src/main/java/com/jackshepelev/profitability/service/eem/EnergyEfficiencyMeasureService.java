3fbbd1f65fce2e663a9a53282ba20818d6869589
Dmytro Kovalenko <dkovalenko@4tifier.com>
2018-11-20 12:20:55 +0200

    add PayOff NPV NPVQ

--- src/main/java/com/jackshepelev/profitability/service/eem/EnergyEfficiencyMeasureService.java
+++ src/main/java/com/jackshepelev/profitability/service/eem/EnergyEfficiencyMeasureService.java
@@ -19,6 +19,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 import java.util.Optional;
@@ -82,8 +83,31 @@ public class EnergyEfficiencyMeasureService extends AbstractService<EnergyEffici
         measure.getResultEEMData().setPayBack(
                 calculatePayBack(measure.getInputEEMData().getInitialInvestment(), measure.getResultEEMData().getNetSavings())
         );
+        measure.getResultEEMData().setPayOff(
+                calculatePayOff(
+                        measure.getResultEEMData().getNetSavings(),
+                        measure.getInputEEMData().getInitialInvestment(),
+                        project.getDiscountRate().getRealDiscountRate(),
+                        measure.getInputEEMData().getEconomicLifeTime()
+                )
+        );
+        measure.getResultEEMData().setNetPresentValue(
+                calculateNPV(
+                        measure.getResultEEMData().getNetSavings(),
+                        project.getDiscountRate().getRealDiscountRate(),
+                        measure.getInputEEMData().getEconomicLifeTime(),
+                        measure.getInputEEMData().getInitialInvestment()
+                )
+        );
 
-
+        measure.getResultEEMData().setNetPresentValueQuotient(
+                calculateNPVQ(
+                        measure.getResultEEMData().getNetSavings(),
+                        project.getDiscountRate().getRealDiscountRate(),
+                        measure.getInputEEMData().getEconomicLifeTime(),
+                        measure.getInputEEMData().getInitialInvestment()
+                )
+        );
 
         measure.setProject(project);
 
@@ -91,11 +115,75 @@ public class EnergyEfficiencyMeasureService extends AbstractService<EnergyEffici
         return repository.save(measure);
     }
 
-    private BigDecimal calculatePayBack(BigDecimal initialInvestment, BigDecimal netSavings) {
+    private BigDecimal calculateNPVQ(BigDecimal netSavings,
+                                     BigDecimal realDiscountRate,
+                                     int economicLifeTime,
+                                     BigDecimal initialInvestment) {
+
+        BigDecimal npv =calculateNPV(netSavings, realDiscountRate, economicLifeTime,initialInvestment);
+        return npv.divide(initialInvestment, 3, RoundingMode.CEILING);
+    }
+
+    private BigDecimal calculateNPV(BigDecimal netSavings,
+                                    BigDecimal realDiscountRate,
+                                    int economicLifeTime,
+                                    BigDecimal initialInvestment) {
+
+        List<BigDecimal> npvList =calculateNPVList(netSavings, realDiscountRate, economicLifeTime);
+        BigDecimal result = BigDecimal.valueOf(0);
+        for (BigDecimal value : npvList) {
+            result = result.add(value);
+        }
+        return result.subtract(initialInvestment);
+    }
+
+    private BigDecimal calculatePayOff(BigDecimal netSavings,
+                                       BigDecimal initialInvestment,
+                                       BigDecimal realDiscountRate,
+                                       int economicLifeTime) {
+
+        List<BigDecimal> npvList =calculateNPVList(netSavings, realDiscountRate, economicLifeTime);
+
+        BigDecimal sum = BigDecimal.valueOf(0);
+        BigDecimal currentValue = null;
+        int index = 0;
+        for (int i = 0; i < npvList.size(); i++) {
+            sum = sum.add(npvList.get(i));
+            currentValue = npvList.get(i);
+            index = i;
+            if (sum.compareTo(initialInvestment) >= 0) {
+                break;
+            }
+        }
+
+        return  BigDecimal.valueOf(index)
+                .add(BigDecimal.valueOf(1)
+                .subtract((sum.subtract(initialInvestment)).divide(currentValue, 3, RoundingMode.CEILING)));
+    }
+
+    private List<BigDecimal> calculateNPVList(BigDecimal netSavings,
+                                          BigDecimal realDiscountRate,
+                                          int economicLifeTime) {
+
+        List<BigDecimal> result = new ArrayList<>();
+        BigDecimal yearValue;
+        for (int i = 1; i <= economicLifeTime; i++) {
+            yearValue = netSavings.divide((BigDecimal.valueOf(1).add(realDiscountRate)).pow(i), 3, RoundingMode.CEILING);
+            result.add(yearValue);
+        }
+
+        return result;
+    }
+
+    private BigDecimal calculatePayBack(BigDecimal initialInvestment,
+                                        BigDecimal netSavings) {
+
         return initialInvestment.divide(netSavings,3, RoundingMode.CEILING);
     }
 
-    private BigDecimal calculateInitialSavings(List<EnergyEfficiency> energyEfficiencies, List<EnergyTariff> energyTariffs) {
+    private BigDecimal calculateInitialSavings(List<EnergyEfficiency> energyEfficiencies,
+                                               List<EnergyTariff> energyTariffs) {
+
         BigDecimal result = BigDecimal.valueOf(0);
         for (EnergyEfficiency efficiency : energyEfficiencies){
             EnergyTariff tariff = energyTariffs
